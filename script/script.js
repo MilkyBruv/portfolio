d208e322db70cceded2834051f738acbbc1d7291
404-WasFound <malachi.mcenroe@gmail.com>
2022-08-20 19:08:50 +1200

    updated site

---
+++ script/script.js
@@ -0,0 +1,22 @@
+const finalIntroText = ["K", "a", "i ", "M", "c", "E", "n", "r", "o", "e"];
+let introText = "";
+
+function sleep(ms) {
+
+    return new Promise(resolve => setTimeout(resolve, ms));
+
+}
+
+async function start() {
+
+    for (let i = 0; i < finalIntroText.length; i++) {
+        
+        introText += finalIntroText[i];
+        document.getElementById("intro-text-header").innerHTML = introText;
+        await sleep(100);
+        
+    }
+
+}
+
+window.onload = start;
